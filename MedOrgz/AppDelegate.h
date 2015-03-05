ba0b7fa7351eb12b35fc417e77b4378e47a96b64
muhtasim3504 <muhtasim_ayaz@yahoo.com>
2015-03-05 12:05:07 -0800

    Initial Commit

---
+++ MedOrgz/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  MedOrgz
+//
+//  Created by Muhtasim Ayaz on 3/5/15.
+//  Copyright (c) 2015 Muhtasim Ayaz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ MedOrgz/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  MedOrgz
+//
+//  Created by Muhtasim Ayaz on 3/5/15.
+//  Copyright (c) 2015 Muhtasim Ayaz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
